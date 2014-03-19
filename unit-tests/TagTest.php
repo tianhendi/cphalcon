b36a9a848254c4c3109dcd486ac756b7a62c8478
dreamsxin <dreamsxin@qq.com>
2014-03-19 18:59:02 +0800

    Merge 1.3.0

--- unit-tests/TagTest.php
+++ unit-tests/TagTest.php
@@ -147,4 +147,10 @@ HTML;
 		$html = Phalcon\Tag::linkTo(array('http://phalconphp.com/en/', 'Phalcon Home', FALSE, 'query' => array('a' => 1, 'b' => 2)));
 		$this->assertEquals($html, '<a href="http://phalconphp.com/en/?a=1&b2">Phalcon Home</a>');
 	 }
+
+	public function testIssue2142()
+	{
+		$html = \Phalcon\Tag::stylesheetLink(array('css/phalcon.css', 'rel' => 'stylesheet/less'));
+		$this->assertEquals($html, '<link rel="stylesheet/less" type="text/css" href="/css/phalcon.css" />'.PHP_EOL);
+	}
 }
